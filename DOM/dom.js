930f2074df462fd2490cc2e22d97960df972f255
Arun Kudiyal <arunkudiyal@icloud.com>
2021-07-19 17:12:27 +0530

    Event Parameters

--- DOM/dom.js
+++ DOM/dom.js
@@ -84,24 +84,59 @@
 
 
 // INPUT_TEXT EVENTS
-const text = document.querySelector('#text-input')
+// const text = document.querySelector('#text-input')
 
-text.addEventListener('focus', () => {
-    console.log(`FOCUSED ON ME`);
+// text.addEventListener('focus', () => {
+//     console.log(`FOCUSED ON ME`);
+// })
+
+// text.addEventListener('blur', () => {
+//     console.log(`BLURRED ON ME`);
+// })
+
+// text.addEventListener('cut', () => {
+//     console.log(`CUT EVENT`);
+// })
+
+// text.addEventListener('copy', () => {
+//     console.log(`COPY EVENT`);
+// })
+
+// text.addEventListener('paste', () => {
+//     console.log(`PASTE EVENT`);
+// })
+
+
+// EVENT EMITTERS | Event Parameter
+// A parameter which is a parameter to a function which is listening to an event
+// const textInput = document.getElementById('text-input')
+// KeyUp & KeyDown
+// textInput.addEventListener('keyup', () => {
+//     console.log('Key has been relaesed');
+// })
+
+// textInput.addEventListener('keydown', () => {
+//     console.log('Key is being pressed');
+// })
+
+// Access the data that my user is trying to type
+// textInput.addEventListener('keyup', (event) => {
+//     console.log(event.target.value);
+// })
+
+// button = document.getElementById('submitBtn')
+// button.addEventListener('click', (e) => {
+//     // Prevents the default nature of the submit button to submit the form
+//     e.preventDefault();
+//     console.log(e);
+// })
+
+// FUN ACTIVITY WITH THE BOX
+const box = document.getElementById('box')
+box.addEventListener('mouseenter', (e) => {
+    box.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 100)`
 })
 
-text.addEventListener('blur', () => {
-    console.log(`BLURRED ON ME`);
-})
-
-text.addEventListener('cut', () => {
-    console.log(`CUT EVENT`);
-})
-
-text.addEventListener('copy', () => {
-    console.log(`COPY EVENT`);
-})
-
-text.addEventListener('paste', () => {
-    console.log(`PASTE EVENT`);
+box.addEventListener('mouseleave', (e) => {
+    box.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 150)`
 })
