3f2fd770e3838c79c33858c7e3b30e649658222a
Arun Kudiyal <arunkudiyal@icloud.com>
2021-07-29 16:37:05 +0530

    Initial Code

---
+++ ES6_Higher_Order_Array_Methods/scripts.js
@@ -0,0 +1,16 @@
+console.log('Welcome to ES6 Higher Order Array Methods...');
+
+const companies = [
+    {name: 'Company One', category: 'Finance', start: 1981, end: 2003},
+    {name: 'Company Two', category: 'Retail', start: 1992, end: 2008},
+    {name: 'Company Three', category: 'Auto', start: 1999, end: 2007},
+    {name: 'Company Four', category: 'Retail', start: 1989, end: 2010},
+    {name: 'Company Five', category: 'Technology', start: 2009, end: 2014},
+    {name: 'Company Six', category: 'Finance', start: 1987, end: 2010},
+    {name: 'Company Seven', category: 'Auto', start: 1986, end: 1996},
+    {name: 'Company Eight', category: 'Technology', start: 2001, end: 2016},
+    {name: 'Company Nine', category: 'Retail', start: 1981, end: 1989},
+]
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32]
+
