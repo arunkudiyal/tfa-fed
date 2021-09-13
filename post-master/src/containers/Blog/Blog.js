609e7e59213efe49a7671fd6f7125cf3e9096ca5
Arun Kudiyal <arunkudiyal@icloud.com>
2021-09-13 18:17:52 +0530

    React Router & React Router DOM

--- post-master/src/containers/Blog/Blog.js
+++ post-master/src/containers/Blog/Blog.js
@@ -1,52 +1,35 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import Posts from '../Blog/Posts/Posts'
+import { Route } from 'react-router';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
     // Initializing empty state
     state = {
-        posts: [],
         selectedPostId: null
     }
 
-    componentDidMount() {
-        // How to make a request | Axios - PROMISES
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4)
-                const updatedPost = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Mona'
-                    }
-                })
-                this.setState( {posts: updatedPost} )
-            })
-            .catch(err => console.log(err))
-    }
-
-    postSelected = (id) => {
-        this.setState({selectedPostId: id})
-    }
-
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={() => this.postSelected(post.id)} />
-        })
-
         return (
             <div>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <header className="Blog">
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+
+                {/* <Posts /> */}
+
+                {/* localhost:3000/new-post */}
+                {/* <Route path="/" exact render={() => <h1>Welcome Home</h1>} /> */}
+                <Route path="/" exact component={Posts} />
+
                 <section>
                     <FullPost id={this.state.selectedPostId} />
                 </section>
---
+++ post-master/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Post from '../../../components/Post/Post'
+import axios from 'axios'
+import './Posts.css'
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        // How to make a request | Axios - PROMISES
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4)
+                const updatedPost = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Mona'
+                    }
+                })
+                this.setState( {posts: updatedPost} )
+            })
+            .catch(err => console.log(err))
+    }
+
+    postSelected = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelected(post.id)} />
+        })
+
+        return(
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+}
+
+export default Posts;
